0d2917e5c0e2464290d96c0676df12cdcf70b682
qork13 <qork13@gmail.com>
2022-02-03 17:17:06 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void BuildTutorialMenu() {
+
+}
+
+int main() {
+    char userOption;
+
+    cout << "Welcome to the BlackJack Tutorial. Please select an option from the menu: "<< endl;
+
+    if(userOption == '?') {
+        BuildTutorialMenu();
+    }
+
+    return 0;
+}
